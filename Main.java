ab4bf18ba823d863904feac20eece67f90472265
Ernesto Perez <eperez@facturas.com.gt>
2026-04-26 17:15:29 -0600

    Proyecto vacio

--- Main.java
+++ Main.java
@@ -6,24 +6,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) {
-		   FileWriter myWriter;
-
-			try {
-				// abren la conexion a la base de datos
-				myWriter = new FileWriter("C://filenotfound.txt");
-
-				myWriter.write("Hi, I'm trying to write something.");
-				myWriter.close();
-
-			} catch (IOException e) {
-
-				System.out.println("Exception: " + e);
-
-			} finally {
-				// cerrar la conexion.
-				System.out.println("End of execution.");
-
-			}
+		   
 	}
 
 }
